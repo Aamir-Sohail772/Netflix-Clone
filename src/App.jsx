316a0fd5983438763166f9605039256a79e2fca8
Aamir-Sohail772 <aamirsohail772@gmail.com>
2024-03-30 11:26:43 +0530

    error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -55,7 +55,7 @@ function App() {
     <BrowserRouter>
       <Header />
       <Routes>
-          // After 
+          {/* // After  */}
         {/* <Route path="/login" element={<Login />} /> */}
         {/* <Route path="/register" element={<Register />} /> */}
 
